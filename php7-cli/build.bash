bbdb69243f637fce3f4ae2dea0c2fa6f83ae061d
Andy Kirkham <andy.kirkham@logicnow.com>
2016-10-18 22:46:14 +0100

    Update to PHP 7.0.12

--- php7-cli/build.bash
+++ php7-cli/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PHP_VER="7.0.9"
+PHP_VER="7.0.12"
 
-docker build --build-arg PHP_VER=$PHP_VER -t php7-cli:${PHP_VER}.1 .
+docker build --build-arg PHP_VER=$PHP_VER -t php7-cli:${PHP_VER} .
 
