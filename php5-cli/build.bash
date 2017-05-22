9513046ba0f062efe6cfa6591d6fe2d152a15d7c
Andy Kirkham <andy.kirkham@logicnow.com>
2017-05-22 09:59:46 +0100

    Upgrade PHP vers, add Jansson v2.10

--- php5-cli/build.bash
+++ php5-cli/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PHP_VER="5.6.27"
+PHP_VER="5.6.30"
 
 docker build -t php5-cli:${PHP_VER} .
 
--- php7-apache/build.sh
+++ php7-apache/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PHP_VER="7.1.1"
+PHP_VER="7.1.5"
 
 docker build --build-arg PHP_VER=$PHP_VER -t php7-apache:${PHP_VER} .
 
