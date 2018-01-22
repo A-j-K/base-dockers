2cab6e2c4c348333bb70039e7b3fdbcc29864e4a
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-01-22 09:51:19 +0000

    Update to 1.3.43, remove dist and pull jit packages

--- aws-sdk-cpp/build.bash
+++ aws-sdk-cpp/build.bash
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-AWS_SDK_CPP_VER="1.3.42"
+AWS_SDK_CPP_VER_MAJOR="1"
+AWS_SDK_CPP_VER_MINOR="3"
+AWS_SDK_CPP_VER_PATCH="43"
+AWS_SDK_CPP_VERSION="${AWS_SDK_CPP_VER_MAJOR}.${AWS_SDK_CPP_VER_MINOR}.${AWS_SDK_CPP_VER_PATCH}"
 
 docker build \
-	--build-arg AWS_SDK_CPP_VER=$AWS_SDK_CPP_VER \
-	--tag aws-sdk-cpp:${AWS_SDK_CPP_VER} \
+	--build-arg AWS_SDK_CPP_VER_MAJOR="1" \
+	--build-arg AWS_SDK_CPP_VER_MINOR="3" \
+	--build-arg AWS_SDK_CPP_VER_PATCH="43" \
+	--build-arg CMAKE_VER_MAJOR="3" \
+	--build-arg CMAKE_VER_MINOR="10" \
+	--build-arg CMAKE_VER_PATCH="2" \
+	--tag aws-sdk-cpp:${AWS_SDK_CPP_VERSION} \
 	.
 
