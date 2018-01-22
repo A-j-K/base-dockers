c7a1ac5d41fa4ca59aad76d059ab915c4c25c022
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-01-22 08:58:30 +0000

    Update to 1.3.42

--- aws-sdk-cpp/build.bash
+++ aws-sdk-cpp/build.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-AWS_SDK_CPP_VER="1.0.65"
+AWS_SDK_CPP_VER="1.3.42"
 
-docker build --build-arg AWS_SDK_CPP_VER=$AWS_SDK_CPP_VER -t aws-sdk-cpp:${AWS_SDK_CPP_VER} .
+docker build \
+	--build-arg AWS_SDK_CPP_VER=$AWS_SDK_CPP_VER \
+	--tag aws-sdk-cpp:${AWS_SDK_CPP_VER} \
+	.
 
