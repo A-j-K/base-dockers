eb9a4e16106d1214847bcbd2d89d0235e7ebbc25
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-01-22 10:37:05 +0000

    Add extra useful info

--- aws-sdk-cpp/build.bash
+++ aws-sdk-cpp/build.bash
@@ -6,12 +6,17 @@ AWS_SDK_CPP_VER_PATCH="43"
 AWS_SDK_CPP_VERSION="${AWS_SDK_CPP_VER_MAJOR}.${AWS_SDK_CPP_VER_MINOR}.${AWS_SDK_CPP_VER_PATCH}"
 
 docker build \
-	--build-arg AWS_SDK_CPP_VER_MAJOR="1" \
-	--build-arg AWS_SDK_CPP_VER_MINOR="3" \
-	--build-arg AWS_SDK_CPP_VER_PATCH="43" \
+	--build-arg AWS_SDK_CPP_VER_MAJOR=$AWS_SDK_CPP_VER_MAJOR \
+	--build-arg AWS_SDK_CPP_VER_MINOR=$AWS_SDK_CPP_VER_MINOR \
+	--build-arg AWS_SDK_CPP_VER_PATCH=$AWS_SDK_CPP_VER_PATCH \
 	--build-arg CMAKE_VER_MAJOR="3" \
 	--build-arg CMAKE_VER_MINOR="10" \
 	--build-arg CMAKE_VER_PATCH="2" \
 	--tag aws-sdk-cpp:${AWS_SDK_CPP_VERSION} \
 	.
+
+echo "Use the following to update the Docker Hub repository"
+echo "  docker login"
+echo "  docker tag aws-sdk-cpp:${AWS_SDK_CPP_VERSION} andykirkham/aws-sdk-cpp:${AWS_SDK_CPP_VERSION}"
+echo "  docker push andykirkham/aws-sdk-cpp:${AWS_SDK_CPP_VERSION}"
 
