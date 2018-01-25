4243f0528977d7dfff2685eaa0902ac0a964addb
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-01-25 08:55:17 +0000

    Update some versions, redefine the Dockerfiles to make them smaller image size wise

--- proxygen-with-aws-sdk/build.sh
+++ proxygen-with-aws-sdk/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-docker build -t proxygen-base .
+PROXYGEN_VER="2018.01.22.00"
+
+docker build \
+	--build-arg PROXYGEN_VER="$PROXYGEN_VER" \
+	-t proxygen-with-aws-sdk:${PROXYGEN_VER} \
+	.
+
+echo "Use the following to update the Docker Hub repository"
+echo "  docker login"
+echo "  docker tag proxygen-with-aws-sdk:${PROXYGEN_VER} andykirkham/proxygen-with-aws-sdk:${PROXYGEN_VER}"
+echo "  docker push andykirkham/proxygen-with-aws-sdk:${PROXYGEN_VER}"
 
