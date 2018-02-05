6be3e8c384ff767d12a527ed1ce53ffffa057d69
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-02-05 09:00:27 +0000

    Update PHP 7.2.2

--- php7-cli/build.bash
+++ php7-cli/build.bash
@@ -1,12 +1,22 @@
 #!/bin/bash
+#
+# TAG_VER:
+#
+# 1. Initial image
+#
 
+TAG_VER="x"
 PHP_VER="7.2.2"
 
-docker build --build-arg PHP_VER=$PHP_VER -t php7-cli:${PHP_VER} .
+docker build \
+	--build-arg PHP_VER=$PHP_VER \
+	--build-arg TAG_VER=$TAG_VER \
+	-t php7-cli:${PHP_VER}-${TAG_VER} \
+	.
 
 echo "Push container to Docker Hub thus:"
 echo "  docker login"
-echo "  docker tag php7-cli:$PHP_VER andykirkham/php7-cli-developer:$PHP_VER"
-echo "  docker push andykirkham/php7-cli-developer:$PHP_VER"
+echo "  docker tag php7-cli:${PHP_VER}-${TAG_VER} andykirkham/php7-cli-developer:${PHP_VER}-${TAG_VER}"
+echo "  docker push andykirkham/php7-cli-developer:${PHP_VER}-${TAG_VER}"
 
 
