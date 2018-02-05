c0092ddbe59da498a6454340ab47937c02ddbe69
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-02-05 16:21:54 +0000

    Improve the build script for various versions and new EP repo

--- php7-cli/build.bash
+++ php7-cli/build.bash
@@ -3,20 +3,35 @@
 # TAG_VER:
 #
 # 1. Initial image
+# 2. Jansson fix: https://github.com/stellartech/phpjansson/commit/fba83b91e5d2f11b6b5c73ef07799dd6ce852bda
 #
 
-TAG_VER="1"
-PHP_VER="7.2.2"
+TAG_VER="2"
+
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+	PHP_VER_MAJOR="7"
+	PHP_VER_MINOR="2"
+	PHP_VER_PATCH="2"
+else
+	PHP_VER_MAJOR=$1
+	PHP_VER_MINOR=$2
+	PHP_VER_PATCH=$3
+fi
+PHP_VER="${PHP_VER_MAJOR}.${PHP_VER_MINOR}.${PHP_VER_PATCH}"
+IMAGE="php-${PHP_VER_MAJOR}-${PHP_VER_MINOR}-${PHP_VER_PATCH}-${TAG_VER}-cli"
+REPO="andykirkham/php-cli"
+
+echo "Building image ${IMAGE}:${PHP_VER}-${TAG_VER}"
 
 docker build \
 	--build-arg PHP_VER=$PHP_VER \
 	--build-arg TAG_VER=$TAG_VER \
-	-t php7-cli:${PHP_VER}-${TAG_VER} \
+	-t ${IMAGE}:${PHP_VER}-${TAG_VER} \
 	.
 
 echo "Push container to Docker Hub thus:"
 echo "  docker login"
-echo "  docker tag php7-cli:${PHP_VER}-${TAG_VER} andykirkham/php7-cli-developer:${PHP_VER}-${TAG_VER}"
-echo "  docker push andykirkham/php7-cli-developer:${PHP_VER}-${TAG_VER}"
+echo "  docker tag ${IMAGE}:${PHP_VER}-${TAG_VER} ${REPO}:${PHP_VER}-${TAG_VER}"
+echo "  docker push ${REPO}:${PHP_VER}-${TAG_VER}"
 
 
