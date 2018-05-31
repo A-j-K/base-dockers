149420ed8ed66cd05606f79e3df4b6510ebdbb87
Miguel Santiago <miguel-santiago@users.noreply.github.com>
2018-05-31 11:55:09 +0100

    Switched docker image to alpine. (#3)
    Added environment variables to override Root CA path and file names.

--- kafka-certs/build.sh
+++ kafka-certs/build.sh
@@ -3,8 +3,6 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build \
-	--build-arg MAKE_USER=${USER} \
-	--build-arg USER_UID=${UID} \
 	-t ca-signer-platform-kafka:latest \
 	${SCRIPTDIR}
 
