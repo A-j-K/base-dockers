10ebf0b0e92ac9c5d32c47f7a3772e17e0cecad8
Andy Kirkham <andy@spiders-lair.com>
2018-09-04 13:09:48 +0100

    Update to Ubuntu Bionic Beaver because it's OpenOCD package and ARM tools are way more upto date

--- arm-gcc/stm32f7x.sh
+++ arm-gcc/stm32f7x.sh
@@ -2,9 +2,9 @@
 
 docker run -d -P --privileged \
 	-p 4444:4444 \
-	-e "OPENOCD_SCRIPTS=/opt/openocd/scripts" \
+	-e "OPENOCD_SCRIPTS=/usr/share/openocd/scripts" \
 	-t arm-gcc:latest \
-	/opt/openocd/bin/openocd \
+	openocd \
 		-f interface/stlink-v2-1.cfg \
 		-f target/stm32f7x.cfg
 
