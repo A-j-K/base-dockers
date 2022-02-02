cae0accc4f8f563aae5853c22d12980d465dc77d
Andy Kirkham <andy.kirkham@solarwinds.com>
2022-02-02 09:47:03 +0000

    Add old stuff

---
+++ aws-sdk-cpp/build-dyn-core.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ./ver.bash
+
+docker build \
+	--build-arg AWS_SDK_CPP_VER_MAJOR=$AWS_SDK_CPP_VER_MAJOR \
+	--build-arg AWS_SDK_CPP_VER_MINOR=$AWS_SDK_CPP_VER_MINOR \
+	--build-arg AWS_SDK_CPP_VER_PATCH=$AWS_SDK_CPP_VER_PATCH \
+	--build-arg CMAKE_VER_MAJOR="3" \
+	--build-arg CMAKE_VER_MINOR="10" \
+	--build-arg CMAKE_VER_PATCH="2" \
+	--build-arg BUILD_SHARED_LIBS="ON" \
+	-f Dockerfile.core \
+	--tag aws-sdk-cpp-core:${AWS_SDK_CPP_VERSION} \
+	.
+
+echo "Use the following to update the Docker Hub repository"
+echo "  docker login"
+echo "  docker tag aws-sdk-cpp-core:${AWS_SDK_CPP_VERSION} andykirkham/aws-sdk-cpp:core-${AWS_SDK_CPP_VERSION}"
+echo "  docker push andykirkham/aws-sdk-cpp:core-${AWS_SDK_CPP_VERSION}"
+
---
+++ aws-sdk-cpp/build-dyn-s3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./ver.bash
+
+docker build \
+	--build-arg AWS_SDK_CPP_VER_MAJOR=$AWS_SDK_CPP_VER_MAJOR \
+	--build-arg AWS_SDK_CPP_VER_MINOR=$AWS_SDK_CPP_VER_MINOR \
+	--build-arg AWS_SDK_CPP_VER_PATCH=$AWS_SDK_CPP_VER_PATCH \
+	--build-arg CMAKE_VER_MAJOR="3" \
+	--build-arg CMAKE_VER_MINOR="10" \
+	--build-arg CMAKE_VER_PATCH="2" \
+	--build-arg BUILD_SHARED_LIBS="OFF" \
+	--build-arg ENABLE_UNITY_BUILD="ON" \
+	-f Dockerfile.s3 \
+	--tag aws-sdk-cpp-s3:${AWS_SDK_CPP_VERSION} \
+	.
+
+echo "Use the following to update the Docker Hub repository"
+echo "  docker login"
+echo "  docker tag aws-sdk-cpp-s3:${AWS_SDK_CPP_VERSION} andykirkham/aws-sdk-cpp:s3-${AWS_SDK_CPP_VERSION}"
+echo "  docker push andykirkham/aws-sdk-cpp:s3-${AWS_SDK_CPP_VERSION}"
+
